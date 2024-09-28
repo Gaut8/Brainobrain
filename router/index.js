a1ee9d84516a4895c25c596439e828ae4e518b9c
Gaut8 <gauthampj7@gmail.com>
2024-09-28 10:51:07 +0530

    Initial commit

---
+++ router/index.js
@@ -0,0 +1,24 @@
+export default new Router({
+    routes: [
+      {
+        path: '/',
+        name: 'index',
+        component: () => import('@/pages/index.vue')
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/pages/login.vue')
+      },
+      {
+        path: '/certificate',
+        name: 'certificate',
+        component: () => import('@/pages/certificate.vue')
+      }
+    //   {
+    //     path: '/socialmedia',
+    //     name: 'socialmedia',
+    //     component: () => import('@/pages/socialmedia.vue')
+    //   }
+    ]
+  });
